82e9718185a4731cd26a2dd9e6c74df8a01d238d
Javiera Diaz <j.c.d2020@hotmail.com>
2020-11-05 00:25:59 -0300

    Revert "--"
    This reverts commit b717bf6b9cd82013ac326b4fe06531250aa7e693.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TipoAros, Aros
 
 # Register your models here.
+admin.site.register(TipoAros)
+admin.site.register(Aros)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoAros(models.Model):
+    #id
+    forma= models.CharField(max_length=40) 
+    # filtro (ovaldaos, corazon, etc) 
+
+class Aros(models.Model):
+    nombre= models.CharField(max_length=40)
+    color= models.CharField(max_length=30)
+    forma= models.ForeignKey(TipoAros, on_delete=models.CASCADE)
+    tamaño= models.IntegerField()
+    stock= models.IntegerField()
+    tipoPin= models.IntegerField()
+
