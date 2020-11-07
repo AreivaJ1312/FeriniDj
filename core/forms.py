ce052eaf3219f1a15287c35c8f7451bb5e7f59d0
Ignacia06 <73953990+Ignacia06@users.noreply.github.com>
2020-11-07 02:31:06 -0300

    Validaciones

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,12 @@ from .models import Aro, TipoAro
 
 
 class AroForm(ModelForm):
+    nombre= forms.CharField(min_length=2, max_length=90)
+    color= forms.CharField(min_length=4, max_length=30)
+    tamaño= forms.IntegerField(min_value=1, max_value=10)
+    stock= forms.IntegerField(min_value=0, max_value=50)
+    tipoPin= forms.CharField(min_length=3, max_length=20)
+    tipoforma= forms.CharField(min_length=5 ,max_length=50)
     class Meta:
         model = Aro
         fields = ['nombre','color','tamaño','stock','tipoPin','tipoforma']
