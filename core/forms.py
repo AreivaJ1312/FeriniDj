82ca32d42a9aff7d8ef44f76743faf7ebf81acc2
Javiera Diaz <j.c.d2020@hotmail.com>
2020-11-08 16:04:23 -0300

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/Areivaj2020/FeriniDj into main"""
    This reverts commit 34dad5e50b364dd1fbe901f778eddf2ccf95284c.

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,6 @@
 from django  import forms
 from django.forms import ModelForm
 from .models import Aro, TipoAro
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 #validaciones
 class AroForm(ModelForm):
@@ -14,11 +12,4 @@ class AroForm(ModelForm):
 
     class Meta:
         model = Aro
-        fields = ['nombre','color','tamaño','stock','tipoPin','tipoforma','imagen']
-
-
-class CustomUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['first_name','last_name','email','username','password1','password2']
-
+        fields = ['nombre','color','tamaño','stock','tipoPin','tipoforma']
