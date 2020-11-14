d52f0720ee694a0aed67c227bc294ab680740355
Javiera Diaz <j.c.d2020@hotmail.com>
2020-11-14 01:41:33 -0300

    ---

--- Ferini/settings.py
+++ Ferini/settings.py
@@ -132,3 +132,15 @@ STATIC_URL = '/static/'
 MEDIA_URL ="/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 #la ruta en donde quedan almacenadas las imagenes
+
+#variables para enviar email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='feriniprueba@gmail.com'
+EMAIL_HOST_PASSWORD = 'Ferini2020'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+
+
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home, aros, Contacto, ListadoAros, nuevo_aro,modificar_aro, elimimar_aro, registro_usuario
-
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name ="home"),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('modificar_aro/<id>', modificar_aro, name="modificar_aro"),
     path('eliminar_aro/<id>',elimimar_aro, name="eliminar_aro"),
     path('registro_usuario', registro_usuario, name= "registro_usuario"),
+     path('reset/password_reset', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', email_template_name='registration/password_reset_email.html'), name="password_reset"),
+    path('reset/password_reset_done', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name="password_reset_done"),
+    path('reset/password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/password_reset/done', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
