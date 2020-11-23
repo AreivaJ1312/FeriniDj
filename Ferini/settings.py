585194839acc3631ed6b94a6dca93bfcce78a3ce
Javiera Diaz <j.c.d2020@hotmail.com>
2020-11-23 11:03:15 -0300

    actualizacion de api, aun sin consumir json

--- Ferini/settings.py
+++ Ferini/settings.py
@@ -212,4 +212,10 @@ SOCIALACCOUNT_PROVIDERS = {
         'VERSION': 'v2.12',
     }
 }
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- Ferini/urls.py
+++ Ferini/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 
 
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('accounts/', include('allauth.urls')),
     
