13be9585eff882213932a64c25437e43d9e9e0d9
Javiera Diaz <j.c.d2020@hotmail.com>
2020-12-09 10:43:11 -0300

    cambios en apitiempo

---
+++ static/core/Javascript/appTiempo.js
@@ -0,0 +1,62 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+        // destructure propierties
+        const { cityDets, weather } = data;
+
+    //uptade details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;</span>
+        </div>
+     `;
+
+     //update the night/day & icon images
+    const iconSrc = ` img/icons/${weather.WeatherIcon}.svg `;
+    icon.setAttribute('src', iconSrc);
+
+
+     let timeSrc = weather.IsDayTime ? '/core/static/core/img/day.svg' : '/core/static/core/img/night.svg';
+    time.setAttribute('src',timeSrc);
+
+     //remove the d-none class if present
+     if(card.classList.contains('d-none')){
+         card.classList.remove('d-none');
+     }
+};
+
+
+const updateCity = async (city) =>{
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return{ cityDets, weather };
+
+};
+
+cityForm.addEventListener('submit',e =>{
+    //prevent default action
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update de ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+
+});
+
+
+const result = true ? 'value 1' : 'value 2';
+console.log(result);
---
+++ static/core/Javascript/pronostico.js
@@ -0,0 +1,40 @@
+const key ='d4lCENqd5A7laA7gRLMMNzqNR1wkbPXS';
+
+//get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+    return data[0];
+    
+
+};
+
+//get city information
+const getCity = async (city) =>{
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+    return data[0];
+    
+
+};
+
+
+
+//getCity('santiago').then(data => {
+//        return getWeather(data.Key)
+ //   }).then(data => {
+//        console.log(data)
+ //   }).catch(err => console.log(err));
+
+//getWeather("60449");
