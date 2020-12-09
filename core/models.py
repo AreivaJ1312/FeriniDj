6bb32d7c2ebbdd8689fd3e5f9b0626072b75a30e
Ignacia06 <73953990+Ignacia06@users.noreply.github.com>
2020-12-09 12:53:26 -0300

    shell

--- core/models.py
+++ core/models.py
@@ -48,6 +48,7 @@ class Contacto(models.Model):
     def __str__(self):
         return self.Nombre
 
+
 @receiver(user_signed_up)
 def create_user_profile(request, user, **kwargs):
     profile = Profile.objects.create(user=user)
